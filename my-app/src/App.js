97c70e26fde9104944d0f5446c0e46957e6d07ef
Viktor <vtarasoffff@gmail.com>
2024-06-25 16:02:26 +0300

    addition conponents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 const App = () => {
 	return (
-		<div>
-					<Header />
-		<div className="App">
-			Simpl html
-			<ul>
-				<li>html</li>
-				<li>css</li>
-				<li>js</li>
-				<li>react</li>
-			</ul>
-		</div>
+		<div className='app-wrapper'>
+			<Header />
+			<Navbar />
+			<div className='content'>
+				<div>
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3LUPRr1FNJNs0QRcrJTlp6sOASsLDbSeE_g&s" alt="" />
+				</div>
+				<div> ava + descr</div>
+				<div>My posts
+					<div>new post</div>
+					<div>Post1</div>
+					<div>Post2</div>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-const Header = () => {
-	return (
-		<div>
-			<a href='#'>Header</a>
-		</div>
-	
-	);
-}
 export default App;
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2020/06/1200/675/TESLA-LOGO.jpg?ve=1&tl=1'></img>
+    </header>
+    );
+}
+
+export default Header;
